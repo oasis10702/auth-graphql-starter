8d22e385fddd8aa44cf5685841d51e7eb87a1b66
oasis10702 <oasis10702@gmail.com>
2022-03-08 21:05:17 +0900

    Define user type

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,6 +1,11 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString } = graphql;
 
-const UserType = new GraphQLObjectType({});
+const UserType = new GraphQLObjectType({
+  name: "UserType",
+  fields: {
+    email: { type: GraphQLString },
+  },
+});
 
 module.exports = UserType;
