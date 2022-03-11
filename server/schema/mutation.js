bc174c6a26f69e1c60c823969979807cd0fd68e8
oasis10702 <oasis10702@gmail.com>
2022-03-11 19:52:24 +0900

    Add new mutation

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -1,1 +1,11 @@
-// @TODO
+const graphql = require("graphql");
+const { module } = require("../../webpack.config");
+const { GraphQLObjectType, GraphQLString } = graphql;
+const UserType = require("./types/user_type");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {},
+});
+
+module.export = mutation;
