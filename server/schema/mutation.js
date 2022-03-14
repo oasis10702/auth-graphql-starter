a06b022fda83b41c734159a41472b3e0da5f021e
oasis10702 <oasis10702@gmail.com>
2022-03-14 21:22:34 +0900

    Fix

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -12,7 +12,7 @@ const mutation = new GraphQLObjectType({
         email: { type: GraphQLString },
         password: { type: GraphQLString },
       },
-      resolve(parentValue, args, context) {},
+      resolve(parentValue, args, request) {},
     },
   },
 });
